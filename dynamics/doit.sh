8bb238e03cf4dd65cedfeda8fcfa824cfb6979a8
Tigran Aivazian <aivazian.tigran@gmail.com>
2017-01-05 11:18:17 +0000

    Print the value of the working directory

--- dynamics/doit.sh
+++ dynamics/doit.sh
@@ -1,10 +1,11 @@
 workdir=$(mktemp -d ${TMPDIR:-/tmp}/solanim.XXXX)
+echo "workdir=$workdir"
 INIT_FILE=$workdir/init.npz
 SOL_FILE_QUANTUM=$workdir/solq.npz
 SOL_FILE_CLASSICAL=$workdir/solq.npz
 
-python3 mkinit.py -x1 -4.0 -x2 4.0 -Nx 1024 \
-                  -p1 -4.0 -p2 4.0 -Np 1024 \
+python3 mkinit.py -x1 -5.0 -x2 5.0 -Nx 128 \
+                  -p1 -4.0 -p2 4.0 -Np 512 \
                   -t1 0.0 -t2 6.283185307179586 -tol 0.01 \
                   -f0 f0-gauss.py -u U_osc_nonrel.py -o $INIT_FILE
 
@@ -12,4 +13,4 @@ python3 prinit.py -i $INIT_FILE
 #python3 solve.py -i $INIT_FILE -o $SOL_FILE_QUANTUM
 python3 solve.py -i $INIT_FILE -o $SOL_FILE_CLASSICAL -c
 python3 solanim.py -d $workdir/frames -i $INIT_FILE -s $SOL_FILE_QUANTUM
-ffmpeg -loglevel quiet -y -r 25 -f image2 -i $workdir/frames/%05d.png -f mp4 -q:v 0 -vcodec libx264 -r 25 anim.mp4 
+ffmpeg -loglevel quiet -y -r 25 -f image2 -i $workdir/frames/%05d.png -f mp4 -q:v 0 -vcodec libx264 -r 25 osc_rel_classical.mp4 
