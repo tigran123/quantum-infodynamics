521e6d3995743f1751696dd775a0a4d168436ab9
Tigran Aivazian <aivazian.tigran@gmail.com>
2017-01-06 11:05:05 +0000

    Animate multiple solutions simultaneously

--- dynamics/solanim.py
+++ dynamics/solanim.py
@@ -16,7 +16,7 @@ mplt.rc('font', family='serif', size=12)
 
 p = arg.ArgumentParser(description="Solution Animator")
 p.add_argument("-i", action="store", help="Initial data filename", dest="ifilename", required=True)
-p.add_argument("-s", action="store", help="Solution data filename", dest="sfilename", required=True)
+p.add_argument("-s", action="append", help="Solution data filename (may be specified more than once)", dest="sfilenames", required=True, default=[])
 p.add_argument("-d", action="store", help="Frames directory", dest="framedir", required=True)
 args = p.parse_args()
 
@@ -29,17 +29,28 @@ with load(args.ifilename) as idata:
     (Hmin,Hmax) = params[-2:]
     U = idata['U']; H = idata['H']
 
-with load(args.sfilename) as data:
-    t = data['t']; W = data['W']; rho = data['rho']; phi = data['phi']
-    (Wmin,Wmax,rho_min,rho_max,phi_min,phi_max) = data['params']
+t = []; W = []; rho = []; phi = []; Wmin = []; Wmax = []; rho_min = []; rho_max = []; phi_min = []; phi_max = []
+Wlevels = []
+Wticks = []
+
+for sfilename in args.sfilenames:
+    with load(sfilename) as data:
+        t.append(data['t']); W.append(data['W']); rho.append(data['rho']); phi.append(data['phi'])
+        params = data['params']
+        Wmin.append(params[0]); Wmax.append(params[1])
+        Wlevels.append(linspace(Wmin[-1], Wmax[-1], 100))
+        Wticks.append(linspace(Wmin[-1], Wmax[-1], 10))
+        rho_min.append(params[2]); rho_max.append(params[3])
+        phi_min.append(params[4]); phi_max.append(params[5])
+
+nsol = len(t)
+assert len(W) == nsol and len(rho) == nsol and len(phi) == nsol and len(Wmin) == nsol and len(Wmin) == nsol and len(Wmax) == nsol
 
 xv,dx = linspace(x1, x2, Nx, endpoint=False, retstep=True)
 pv,dp = linspace(p1, p2, Np, endpoint=False, retstep=True)
 xx,pp = mgrid[x1:x2-dx:Nx*1j, p1:p2-dp:Np*1j]
 
 Hlevels = linspace(Hmin, Hmax, 10)
-Wlevels = linspace(Wmin, Wmax, 100)
-Wticks = linspace(Wmin, Wmax, 10)
 
 def fmt(x, pos):
     return "%3.2f" % x
@@ -62,43 +73,55 @@ class MidpointNormalize(Normalize):
 
 norm = MidpointNormalize(midpoint=0.0)
 
-for k in range(len(t)):
-    fig, (ax1,ax2,ax3) = plt.subplots(1, 3, figsize=(19.2,10.8), dpi=100)
+t_longest = max(t, key=len)
+s_longest = t.index(t_longest)
+time_steps = len(t_longest)
+
+for k in range(time_steps):
+    fig, axes = plt.subplots(nsol, 3, figsize=(19.2,10.8), dpi=100)
     
-    ax1.contour(xx, pp, H, levels=Hlevels, linewidths=0.5, colors='k')
-    ax1.set_title("Information field $W(x,p,t)$")
-    ax1.grid(True)
-    im1 = ax1.contourf(xx, pp, W[k], levels=Wlevels, norm=norm, cmap=cm.bwr)
-    divider = make_axes_locatable(ax1)
-    cax = divider.append_axes("right", "2%", pad="1%")
-    plt.colorbar(im1, cax = cax, ticks=Wticks, format=mplt.ticker.FuncFormatter(fmt))
-    ax1.set_ylabel(r'$p$')
-    ax1.set_xlabel(r'$x$')
-    ax1.set_xlim([x1,x2-dx])
-    ax1.set_ylim([p1,p2-dp])
+    print("Time index k=", k)
+    s = 0
+    for ax in axes:
+        if s == s_longest:
+            time_index = k
+        else:
+            time_index = int(k*len(t[s])/time_steps)
+        ax[0].contour(xx, pp, H, levels=Hlevels, linewidths=0.5, colors='k')
+        ax[0].set_title("Information field $W(x,p,t)$")
+        ax[0].grid(True)
+        im = ax[0].contourf(xx, pp, W[s][time_index], levels=Wlevels[s], norm=norm, cmap=cm.bwr)
+        divider = make_axes_locatable(ax[0])
+        cax = divider.append_axes("right", "2%", pad="1%")
+        plt.colorbar(im, cax = cax, ticks=Wticks[s], format=mplt.ticker.FuncFormatter(fmt))
+        ax[0].set_ylabel('$p$')
+        ax[0].set_xlabel('$x$')
+        ax[0].set_xlim([x1,x2-dx])
+        ax[0].set_ylim([p1,p2-dp])
 
-    ax2.set_title(r"Spatial density $\rho(x,t)$")
-    ax2.grid(True)
-    rho_now = rho[k]
-    ax2.plot(xv, rho_now, color='black')
-    ax2.fill_between(xv, 0, rho_now, where=rho_now>0, color='red', interpolate=True)
-    ax2.fill_between(xv, 0, rho_now, where=rho_now<0, color='blue', interpolate=True)
-    ax2.set_ylabel(r'$\rho$')
-    ax2.set_xlabel(r'$x$')
-    ax2.set_xlim([x1,x2-dx])
-    ax2.text(3.0, 3.2, "t=% 6.3f" % t[k])
-    ax2.set_ylim([1.03*rho_min,1.03*rho_max])
+        ax[1].set_title(r"Spatial density $\rho(x,t)$")
+        ax[1].grid(True)
+        rho_now = rho[s][time_index]
+        ax[1].plot(xv, rho_now, color='black')
+        ax[1].fill_between(xv, 0, rho_now, where=rho_now>0, color='red', interpolate=True)
+        ax[1].fill_between(xv, 0, rho_now, where=rho_now<0, color='blue', interpolate=True)
+        ax[1].set_ylabel(r'$\rho$')
+        ax[1].set_xlabel('$x$')
+        ax[1].set_xlim([x1,x2-dx])
+        ax[1].text(3.0, 3.2, "t=% 6.3f" % t[s][time_index])
+        ax[1].set_ylim([1.02*rho_min[s],1.02*rho_max[s]])
 
-    ax3.set_title(r"Momentum density $\varphi(p,t)$")
-    ax3.grid(True)
-    phi_now = phi[k]
-    ax3.plot(pv, phi_now, color='black')
-    ax3.fill_between(pv, 0, phi_now, where=phi_now>0, color='red', interpolate=True)
-    ax3.fill_between(pv, 0, phi_now, where=phi_now<0, color='blue', interpolate=True)
-    ax3.set_ylabel(r'$\varphi$')
-    ax3.set_xlabel(r'$p$')
-    ax3.set_xlim([p1,p2-dp])
-    ax3.set_ylim([1.03*phi_min,1.03*phi_max])
+        ax[2].set_title(r"Momentum density $\varphi(p,t)$")
+        ax[2].grid(True)
+        phi_now = phi[s][time_index]
+        ax[2].plot(pv, phi_now, color='black')
+        ax[2].fill_between(pv, 0, phi_now, where=phi_now>0, color='red', interpolate=True)
+        ax[2].fill_between(pv, 0, phi_now, where=phi_now<0, color='blue', interpolate=True)
+        ax[2].set_ylabel(r'$\varphi$')
+        ax[2].set_xlabel('$p$')
+        ax[2].set_xlim([p1,p2-dp])
+        ax[2].set_ylim([1.02*phi_min[s],1.02*phi_max[s]])
+        s += 1
 
     plt.tight_layout()
     fig.savefig(framedir + '/%05d.png' % k)
