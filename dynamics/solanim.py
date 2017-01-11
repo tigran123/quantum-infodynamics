7a8b313b9b1b7463286e55e1c43da9b1c815393a
Tigran Aivazian <aivazian.tigran@gmail.com>
2017-01-11 23:25:08 +0000

    Move mkinit functionality into the solver

--- dynamics/solanim.py
+++ dynamics/solanim.py
@@ -13,8 +13,7 @@ from argparse import ArgumentParser as argp
 mplt.rc('font', family='serif', size=12)
 
 p = argp(description="Solution Animator")
-p.add_argument("-i", action="store", help="Initial data filename", dest="ifilename", required=True)
-p.add_argument("-s", action="append", help="Solution data filename (may be specified more than once)", dest="sfilenames", required=True, default=[])
+p.add_argument("-s", action="append", help="Solution data filename (multiple OK)", dest="sfilenames", required=True, default=[])
 p.add_argument("-P", action="store", help="Number of parts to split the time range into", dest="nparts", type=int, default=1)
 p.add_argument("-p", action="store", help="The part number to process in this instance", dest="part", type=int, default=1)
 p.add_argument("-d", action="store", help="Frames directory", dest="framedir", required=True)
@@ -31,22 +30,14 @@ def pr_exit(str):
 if nparts <= 0: pr_exit("Number of parts must be positive, but %d <= 0" % nparts)
 if part <= 0 or part > nparts: pr_exit("The part number must lie between 1 and %d,  but %d <= 0" % (nparts, part))
 
-with load(args.ifilename) as idata:
-    params = idata['params'][()] # very mysterious indexing! ;)
-    x1 = params['x1']; x2 = params['x2']; Nx = params['Nx']
-    p1 = params['p1']; p2 = params['p2']; Np = params['Np']
-    Hmin = params['Hmin']; Hmax = params['Hmax']
-    U = idata['U']; H = idata['H']
-
-t = []; W = []; rho = []; phi = []; Wmin = []; Wmax = []; rho_min = []; rho_max = []; phi_min = []; phi_max = []
-Wlevels = []
-Wticks = []
-Wfilenames = []
-Nt = []
+t,Nt,W,rho,phi,Wmin,Wmax,rho_min,rho_max,phi_min,phi_max = [],[],[],[],[],[],[],[],[],[],[]
+Wlevels,Wticks,Wfilenames = [],[],[]
+x1,x2,Nx,p1,p2,Np = [],[],[],[],[],[]
+H,Hmin,Hmax = [],[],[]
 
 for sfilename in args.sfilenames:
     with load(sfilename) as data:
-        t.append(data['t']); rho.append(data['rho']); phi.append(data['phi'])
+        t.append(data['t']); rho.append(data['rho']); phi.append(data['phi']); H.append(data['H'])
         params = data['params'][()]
         Wmin.append(params['Wmin']); Wmax.append(params['Wmax'])
         Wlevels.append(linspace(Wmin[-1], Wmax[-1], 100))
@@ -55,14 +46,18 @@ for sfilename in args.sfilenames:
         phi_min.append(params['phi_min']); phi_max.append(params['phi_max'])
         Wfilenames.append(params['Wfilename'])
         Nt.append(params['Nt'])
+        x1.append(params['x1']); x2.append(params['x2']); Nx.append(params['Nx'])
+        p1.append(params['p1']); p2.append(params['p2']); Np.append(params['Np'])
+        Hmin.append(params['Hmin']); Hmax.append(params['Hmax'])
 
-W = [memmap(filename, mode='r', dtype='float64', shape=(nt,Nx,Np)) for (filename,nt) in zip(Wfilenames, Nt)]
+W = [memmap(filename, mode='r', dtype='float64', shape=(nt,nx,np)) for (filename,nt,nx,np) in zip(Wfilenames, Nt,Nx,Np)]
 
-xv,dx = linspace(x1, x2, Nx, endpoint=False, retstep=True)
-pv,dp = linspace(p1, p2, Np, endpoint=False, retstep=True)
-xx,pp = mgrid[x1:x2-dx:Nx*1j, p1:p2-dp:Np*1j]
-
-Hlevels = linspace(Hmin, Hmax, 10)
+xvdx = [linspace(x1i, x2i, Nxi, endpoint=False, retstep=True) for (x1i,x2i,Nxi) in zip(x1,x2,Nx)]
+pvdp = [linspace(p1i, p2i, Npi, endpoint=False, retstep=True) for (p1i,p2i,Npi) in zip(p1,p2,Np)]
+dx = [a[1] for a in xvdx]
+dp = [a[1] for a in pvdp]
+xxpp = [mgrid[x1i:x2i-dxi:Nxi*1j, p1i:p2i-dpi:Npi*1j] for (x1i,x2i,dxi,Nxi,p1i,p2i,dpi,Npi) in zip(x1,x2,dx,Nx,p1,p2,dp,Np)]
+Hlevels =  [linspace(hmin, hmax, 10) for (hmin,hmax) in zip(Hmin,Hmax)]
 
 def fmt(x, pos):
     return "%3.2f" % x
@@ -108,11 +103,14 @@ for k in time_range:
     if nsol == 1: axes_list = [axes]
     else: axes_list = axes
     for ax in axes_list:
+        xx,pp = xxpp[s][0],xxpp[s][1]
+        xv = xvdx[s][0]
+        pv = pvdp[s][0]
         if s == s_longest:
             time_index = k
         else: # find an element in t[s] closest to the current time value (i.e. t_longest[k])
             time_index = abs(t[s] - t_longest[k]).argmin()
-        ax[0].contour(xx, pp, H, levels=Hlevels, linewidths=0.5, colors='k')
+        ax[0].contour(xx, pp, H[s], levels=Hlevels[s], linewidths=0.5, colors='k')
         ax[0].set_title("Information field $W(x,p,t)$")
         im = ax[0].contourf(xx, pp, W[s][time_index], levels=Wlevels[s], norm=norm, cmap=cm.bwr)
         divider = make_axes_locatable(ax[0])
@@ -120,8 +118,8 @@ for k in time_range:
         plt.colorbar(im, cax = cax, ticks=Wticks[s], format=mplt.ticker.FuncFormatter(fmt))
         ax[0].set_ylabel('$p$')
         ax[0].set_xlabel('$x$')
-        ax[0].set_xlim([x1,x2-dx])
-        ax[0].set_ylim([p1,p2-dp])
+        ax[0].set_xlim([x1[s],x2[s]-dx[s]])
+        ax[0].set_ylim([p1[s],p2[s]-dp[s]])
 
         ax[1].set_title(r"Spatial density $\rho(x,t)$")
         rho_now = rho[s][time_index]
@@ -130,7 +128,7 @@ for k in time_range:
         ax[1].fill_between(xv, 0, rho_now, where=rho_now<0, color='blue', interpolate=True)
         ax[1].set_ylabel(r'$\rho$')
         ax[1].set_xlabel('$x$')
-        ax[1].set_xlim([x1,x2-dx])
+        ax[1].set_xlim([x1[s],x2[s]-dx[s]])
         ax[1].text(0.8, 0.8, "t=% 6.3f" % t[s][time_index], transform=ax[1].transAxes)
         ax[1].set_ylim([1.02*rho_min[s],1.02*rho_max[s]])
 
@@ -141,7 +139,7 @@ for k in time_range:
         ax[2].fill_between(pv, 0, phi_now, where=phi_now<0, color='blue', interpolate=True)
         ax[2].set_ylabel(r'$\varphi$')
         ax[2].set_xlabel('$p$')
-        ax[2].set_xlim([p1,p2-dp])
+        ax[2].set_xlim([p1[s],p2[s]-dp[s]])
         ax[2].set_ylim([1.02*phi_min[s],1.02*phi_max[s]])
         s += 1
 
--- dynamics/solve.py
+++ dynamics/solve.py
@@ -4,9 +4,10 @@
    License: GPL
 """
 
-from numpy import load, exp, zeros, real, savez, amin, amax, sum, abs, memmap
+from numpy import linspace, mgrid, pi, newaxis, exp, real, savez, amin, amax, sum, abs, memmap
 import argparse as arg
 from time import time
+from os.path import isfile, splitext
 
 # select FFT implementation
 #from numpy.fft import ifftshift, fftshift, fft, ifft
@@ -17,7 +18,16 @@ pyfftw.interfaces.cache.enable()
 pyfftw.interfaces.cache.set_keepalive_time(10)
 
 p = arg.ArgumentParser(description="Quantum Infodynamics Solver")
-p.add_argument("-i", action="store", help="Initial data file name", dest="ifilename", required=True)
+p.add_argument("-x1", action="store", help="Starting coordinate", dest="x1", type=float, required=True)
+p.add_argument("-x2", action="store", help="Final coordinate", dest="x2", type=float, required=True)
+p.add_argument("-Nx", action="store", help="Number of points in x direction", dest="Nx", type=int, required=True)
+p.add_argument("-p1", action="store", help="Starting momentum", dest="p1", type=float, required=True)
+p.add_argument("-p2", action="store", help="Final momentum", dest="p2", type=float, required=True)
+p.add_argument("-Np", action="store", help="Number of points in p direction", dest="Np", type=int, required=True)
+p.add_argument("-t1", action="store", help="Starting time", dest="t1", type=float, required=True)
+p.add_argument("-t2", action="store", help="Final time", dest="t2", type=float, required=True)
+p.add_argument("-f0", action="store", help="Python source of f0(x,p)", dest="srcf0", required=True)
+p.add_argument("-u",  action="store", help="Python source of U(x), T(p), U'(x) and T'(p)", dest="srcU", required=True)
 p.add_argument("-o", action="store", help="Solution file name", dest="ofilename", required=True)
 p.add_argument("-W", action="store", help="Solution W(x,p,t) file name", dest="Wfilename", required=True)
 p.add_argument("-c", action="store_true", help="Use classical (non-quantum) propagator", dest="classical")
@@ -28,23 +38,64 @@ def pr_exit(str):
     print("ERROR:" + str)
     exit()
 
-tol = args.tol
+srcf0 = args.srcf0; srcU = args.srcU
+(x1,x2,Nx,p1,p2,Np,t1,t2,tol) = (args.x1,args.x2,args.Nx,args.p1,args.p2,args.Np,args.t1,args.t2,args.tol)
 if tol <= 0: pr_exit("Tolerance value must be positive, but %f <=0" % tol)
+if not isfile(srcf0): pr_exit("No such file '%s'" %(srcf0))
+if not isfile(srcU): pr_exit("No such file '%s'" %(srcU))
+if x2 <= x1: pr_exit("x2 must be greater than x1, but %f <= %f" %(x2,x1))
+if p2 <= p1: pr_exit("p2 must be greater than p1, but %f <= %f" %(p2,p1))
+if t2 <= t1: pr_exit("t2 must be greater than t1, but %f <= %f" %(t2,t1))
+if Nx <= 0: pr_exit("Nx must be positive, but %d <= 0" % Nx)
+if Np <= 0: pr_exit("Np must be positive, but %d <= 0" % Np)
+if Nx & (Nx-1): print("WARNING: Nx=%d is not a power 2, FFT may be slowed down" % Nx)
+if Np & (Np-1): print("WARNING: Np=%d is not a power 2, FFT may be slowed down" % Np)
+
+# construct the mesh grid for evaluating f0(x,p), U(x), dUdx(x), T(p), dTdp(p)
+xv,dx = linspace(x1, x2, Nx, endpoint=False, retstep=True)
+pv,dp = linspace(p1, p2, Np, endpoint=False, retstep=True)
+xx,pp = mgrid[x1:x2-dx:Nx*1j,p1:p2-dp:Np*1j]
+
+# ranges in Fourier image spaces (theta is conjugated to p)
+dtheta = 2.*pi/(p2-p1)
+theta_amp = dtheta*Np/2.
+thetav = linspace(-theta_amp, theta_amp - dtheta, Np)
+
+# lam is conjugated to x
+dlam = 2.*pi/(x2-x1)
+lam_amp = dlam*Nx/2.
+lamv = linspace(-lam_amp, lam_amp - dlam, Nx)
+
+# now shift them all to center zero frequency
+X = fftshift(xv)[:,newaxis]
+P = fftshift(pv)[newaxis,:]
+Theta = fftshift(thetav)[newaxis,:]
+Lam = fftshift(lamv)[:,newaxis]
+
+def qd(f, x, dx):
+    """Quantum differential of function f(x) at a point x on the increment dx"""
+    hbar = 1.0
+    return (f(x+1j*hbar*dx/2.) - f(x-1j*hbar*dx/2.))/(1j*hbar)
+
+# load the python modules with initial distribution and the physical model (U(x) and T(p))
+f0mod = __import__(splitext(srcf0)[0])
+Umod = __import__(splitext(srcU)[0])
+
+if args.classical:
+    method = "CLASS"
+    dU = Umod.dUdx(X)*1j*Theta
+    dT = -Umod.dTdp(P)*1j*Lam/2.
+else:
+    method = "QUANT"
+    dU = qd(Umod.U, X, 1j*Theta)
+    dT = qd(Umod.T, P, -1j*Lam)/2.
+
+H = Umod.T(pp)+Umod.U(xx)
+x0 = f0mod.x0
+p0 = f0mod.p0
 
 t_start = time()
-with load(args.ifilename) as data:
-    params = data['params'][()] # very mysterious indexing! ;)
-    x1 = params['x1']; x2 = params['x2']; Nx = params['Nx']
-    p1 = params['p1']; p2 = params['p2']; Np = params['Np']
-    t1 = params['t1']; t2 = params['t2']
-    W0 = fftshift(data['f0'])
-    if args.classical:
-        method = "Classical"
-        dU = data['cdU']; dT = data['cdT']
-    else:
-        method = "Quantum"
-        dU = data['qdU']; dT = data['qdT']
-print("%s: initial data loaded in %8.2f seconds" % (method, time() - t_start))
+W0 = fftshift(f0mod.f0(xx,pp))
 
 def solve_spectral(Winit, expU, expT):
     B = fft(Winit, axis=0) # (x,p) -> (λ,p)
@@ -102,13 +153,14 @@ W = ifftshift(W, axes=(1,2))
 rho = sum(W, axis=2)*dp
 phi = sum(W, axis=1)*dx
 Nt = len(tv)
-params = {'Wmin': amin(W), 'Wmax': amax(W), 'rho_min': amin(rho), 'rho_max': amax(rho),
-          'phi_min': amin(phi), 'phi_max': amax(phi), 'tol': tol, 'Wfilename': args.Wfilename, 'Nt': Nt}
+params = {'Wmin': amin(W), 'Wmax': amax(W), 'rho_min': amin(rho), 'rho_max': amax(rho), 'Hmin': amin(H), 'Hmax': amax(H),
+          'phi_min': amin(phi), 'phi_max': amax(phi), 'tol': tol, 'Wfilename': args.Wfilename, 'Nt': Nt,
+          'x0': x0, 'p0': p0, 'x1': x1, 'x2': x2, 'Nx': Nx, 'p1': p1, 'p2': p2, 'Np': Np}
 
 print("%s: solved in %8.2f seconds, %d steps" % (method, time() - t_start, Nt))
 
 t_start = time()
-savez(args.ofilename, t=tv, rho=rho, phi=phi, params=params)
+savez(args.ofilename, t=tv, rho=rho, phi=phi, H=H, params=params)
 fp = memmap(args.Wfilename, dtype='float64', mode='w+', shape=(Nt, Nx, Np))
 fp[:] = W[:]
 del fp
