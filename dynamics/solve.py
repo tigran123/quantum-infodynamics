45cd85179e7db2cbb0a04a16a62fe22fc458841a
Tigran Aivazian <aivazian.tigran@gmail.com>
2017-01-31 10:47:00 +0000

    More accurate estimation of iteration left and tidier message output

--- dynamics/solve.py
+++ dynamics/solve.py
@@ -41,8 +41,11 @@ args = p.parse_args()
 sfilename = args.sfilename
 Wfilename = sfilename + '_W.npz'
 
+def pr_msg(str):
+    print(descr + ": " + str)
+
 def pr_exit(str):
-    print("ERROR:" + str)
+    pr_msg("ERROR: " + str)
     exit()
 
 # load the python module with the physical model (U(x) and dUdx(x))
@@ -142,13 +145,13 @@ tv = [t1]
 t = t1
 Nt = 1
 while t <= t2:
-    if Nt%300 == 299: print("%s: step %d"%(descr,Nt))
+    if Nt%300 == 299: pr_msg("%s: step %d"%Nt)
     if Nt%20 == 1:
         (Wnext, new_dt, expU, expT) = adjust_step(dt, W[-1])
         W.append(Wnext)
         if new_dt != dt:
-            est_steps = (t2-t)//new_dt
-            print("%s: step %d, adjusted dt %.4f -> %.4f, estimated %d steps left" %(descr,Nt,dt,new_dt,est_steps))
+            est_steps = (t2-t)//new_dt + 1
+            pr_msg("step %d, dt %.4f -> %.4f, ~%d steps left" %(Nt,dt,new_dt,est_steps))
             dt = new_dt
     else:
         W.append(solve_spectral(W[-1], expU, expT))
@@ -156,7 +159,7 @@ while t <= t2:
     Nt += 1
     tv.append(t)
 
-print("%s: solved in %8.2f seconds, %d steps" % (descr, time() - t_start, Nt))
+pr_msg("solved in %.1fs, %d steps" % (time() - t_start, Nt))
 
 W = ifftshift(W, axes=(1,2))
 rho = sum(W, axis=2)*dp
@@ -177,4 +180,4 @@ savez(sfilename, t=tv, rho=rho, phi=phi, H=H, U=Uv, T=Tv, E=E, params=params)
 fp = memmap(Wfilename, dtype='float64', mode='w+', shape=(Nt, Nx, Np))
 fp[:] = W[:]
 del fp # causes the flush of memmap
-print("%s: solution saved in %8.2f seconds" % (descr, time() - t_start))
+pr_msg("solution saved in %.1fs" % (time() - t_start))
