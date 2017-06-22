34ce40579f8fc462d44f208b3ab1a99032aa096c
Tigran Aivazian <aivazian.tigran@gmail.com>
2017-06-22 08:12:27 +0100

    Add script for composing videos from multiple streams

---
+++ dynamics/bin/combine-free-particles.sh
@@ -0,0 +1,3 @@
+ffmpeg -y -i tachyon.mp4 -i tardyon.mp4 -i luxon.mp4 -filter_complex \
+      '[0:v] pad=2*iw:2*ih [tachyon]; [tachyon][1:v] overlay=main_w/2:0,overlay=0:main_h/2' \
+      out.mp4 && \
