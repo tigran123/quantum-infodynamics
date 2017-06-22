0d571e907ce2d933fe876cef49196cf9048750b2
Tigran Aivazian <aivazian.tigran@gmail.com>
2017-06-22 08:42:53 +0100

    Detect the number of CPUs

--- dynamics/bin/harmonic-oscillator.sh
+++ dynamics/bin/harmonic-oscillator.sh
@@ -1,9 +1,13 @@
 workdir=$(mktemp -d ${TMPDIR:-/tmp}/solanim.XXXX)
+workdir=/tmp/harmonic-oscillator
+rm -rf $workdir ; mkdir -p $workdir/frames
 
 SOLQR=$workdir/solqr
 SOLCR=$workdir/solcr
 SOLQN=$workdir/solqn
 SOLCN=$workdir/solcn
+nproc=$(nproc)
+FPS=20
 
 MOVIE_FILE=harmonic-oscillator.mp4
 PARAMS="-x0 0.0 -p0 1.0 -sigmax 0.2 -sigmap 0.1 -x1 -5.0 -x2 5.0 -Nx 512 -p1 -4.0 -p2 4.0 -Np 512 -t1 0.0 -t2 6.283185307179586 -u U_harmonic"
@@ -14,12 +18,11 @@ python3 solve.py -d "Quantum Non-relativistic Oscillator" $PARAMS -tol 0.01 -s $
 python3 solve.py -c -d "Classical Non-relativistic Oscillator" $PARAMS -tol 0.01 -s $SOLCN &
 wait
 
-#mkdir -p $workdir/frames
-#python3 solanim.py -P 4 -p 1 -d $workdir/frames -s $SOLQR -s $SOLCR -s $SOLQN -s $SOLCN &
-#python3 solanim.py -P 4 -p 2 -d $workdir/frames -s $SOLQR -s $SOLCR -s $SOLQN -s $SOLCN &
-#python3 solanim.py -P 4 -p 3 -d $workdir/frames -s $SOLQR -s $SOLCR -s $SOLQN -s $SOLCN &
-#python3 solanim.py -P 4 -p 4 -d $workdir/frames -s $SOLQR -s $SOLCR -s $SOLQN -s $SOLCN &
-#wait
-#ffmpeg -loglevel quiet -y -r 25 -f image2 -i $workdir/frames/%05d.png -f mp4 -q:v 0 -vcodec libx264 -r 25 $MOVIE_FILE
+for ((i=1; i <= $nproc; i++));
+do
+    python3 solanim.py -P $nproc -p $i -np -d $workdir/frames -s $SOLQR -s $SOLCR -s $SOLQN -s $SOLCN &
+done
+wait
+ffmpeg -loglevel quiet -y -r $FPS -f image2 -i $workdir/frames/%05d.png -f mp4 -q:v 0 -vcodec libx264 -r $FPS $MOVIE_FILE
 
-python3 solplay.py -o $MOVIE_FILE -s $SOLQR -s $SOLCR -s $SOLQN -s $SOLCN
+#python3 solplay.py -o $MOVIE_FILE -s $SOLQR -s $SOLCR -s $SOLQN -s $SOLCN
