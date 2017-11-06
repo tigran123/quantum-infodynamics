202b620fd4936537f815e36d527eee38f84f3e90
Tigran Aivazian <aivazian.tigran@gmail.com>
2017-11-06 11:10:40 +0000

    Oops, wrong python interpreted was executed.

--- dynamics/bin/harmonic-oscillator-solve.sh
+++ dynamics/bin/harmonic-oscillator-solve.sh
@@ -18,8 +18,8 @@ FPS=20
 
 PARAMS="-x0 0.0 -p0 1.0 -sigmax 0.2 -sigmap 0.1 -x1 -5.0 -x2 5.0 -Nx 512 -p1 -4.0 -p2 4.0 -Np 512 -t1 0.0 -t2 6.283185307179586 -u U_harmonic"
 
-python solve.py -r -d "Quantum Relativistic Oscillator" $PARAMS -tol 0.01 -s $SOLQR &
-python3 solve.py -r -c -d "Classical Relativistic Oscillator" $PARAMS -tol 0.01 -s $SOLCR &
-python3 solve.py -d "Quantum Non-relativistic Oscillator" $PARAMS -tol 0.01 -s $SOLQN &
-python3 solve.py -c -d "Classical Non-relativistic Oscillator" $PARAMS -tol 0.01 -s $SOLCN &
+python3 solve.py -r -d "Quantum Relativistic Oscillator" $PARAMS -tol 0.007 -s $SOLQR &
+python3 solve.py -r -c -d "Classical Relativistic Oscillator" $PARAMS -tol 0.0005 -s $SOLCR &
+python3 solve.py -d "Quantum Non-relativistic Oscillator" $PARAMS -tol 0.001 -s $SOLQN &
+python3 solve.py -c -d "Classical Non-relativistic Oscillator" $PARAMS -tol 0.001 -s $SOLCN &
 wait
