482ab87c331e8fe8aed48a0bff82edf34c692d19
Tigran Aivazian <aivazian.tigran@gmail.com>
2017-11-28 17:42:24 +0000

    Use single quotes consistently

--- classical-mechanics/pendulum/pendulum.py
+++ classical-mechanics/pendulum/pendulum.py
@@ -1,33 +1,16 @@
-"""
+'''
   Mathematical Pendulum Simulator (Python Class Definition)
   Author: Tigran Aivazian <aivazian.tigran@gmail.com>
   Released under GPLv3, 2017.
-
-  origin
-   |\
-     \
-   |  \
-       \
-   |    \
-         \ L
-   |      \
-           \
-   |--phi-> \
-             \
-   |          \
-               \
-   |            \
-                 O M
-   |
-"""
+'''
 
 from numpy import sin, cos, pi
 from scipy.integrate import odeint
 
 class Pendulum:
-    """Pendulum Class --- model of a mathematical pendulum
-       We use Lagrangian dynamics in variables (phi, phidot = dphi/dt)
-    """
+    '''Pendulum Class --- model of a mathematical pendulum.
+       Uses Lagrangian dynamics in variables (phi, phidot = dphi/dt)
+    '''
     def __init__(self,
                  phi    = pi, # initial angle phi, in radians
                  phidot =  0.0,  # initial angular velocity = dphi/dt, in radian/s
@@ -48,7 +31,7 @@ class Pendulum:
         self.energy_text = None # matplotlib text artist for the energy value
 
     def position(self):
-        """Return the current position of the pendulum"""
+        '''Return the current position of the pendulum'''
         L = self.L
         phi = self.phi
         x = self.origin[0] + L*sin(phi)
@@ -56,7 +39,7 @@ class Pendulum:
         return [[self.origin[0], x], [self.origin[1],y]]
 
     def Hamiltonian(self, phi, phidot):
-        """Return the total energy (Kinetic+Potential) of the specified state"""
+        '''Return the total energy (Kinetic+Potential) of the specified state'''
         M = self.M
         L = self.L
         G = self.G
@@ -65,21 +48,21 @@ class Pendulum:
         return T + U
 
     def energy(self):
-        """Return the total energy (Kinetic+Potential) of the current state"""
+        '''Return the total energy (Kinetic+Potential) of the current state'''
         return self.Hamiltonian(self.phi, self.phidot)
 
     def derivs(self, state, t):
-        """Return the RHS of the ODEs of motion"""
+        '''Return the RHS of the ODEs of motion'''
         return [state[1], 0.0 if abs(state[0]) == pi else -self.G*sin(state[0])/self.L]
 
     def evolve(self, t1, t2):
-        """Evolve the pendulum from the moment of time t1 to t2"""
+        '''Evolve the pendulum from the moment of time t1 to t2'''
         self.phi,self.phidot = odeint(self.derivs, [self.phi,self.phidot], [t1, t2])[1]
         if self.phi > pi: self.phi -= 2*pi    # the phase space is a cylinder, so we must wrap ...
         elif self.phi < -pi: self.phi += 2*pi # ... phi around to remain within [-pi, pi]
 
     def free(self):
-        """Free the resources held by this instance"""
+        '''Free the resources held by this instance'''
         self.line.remove()
         del(self.line)
         self.energy_text.remove()
