eb646fd0be5c28a6d909961b612a33e822090f48
Tigran Aivazian <aivazian.tigran@gmail.com>
2019-07-04 09:58:03 +0100

    Simple optimisation

--- dynamics/U_harmonic.py
+++ dynamics/U_harmonic.py
@@ -1,14 +1,5 @@
-# Physical model of non-relativistic harmonic oscillator
-
-params = {'$m$': 1.0, '$\omega$': 1.0}
-
-_m = params['$m$']
-_omega = params['$\omega$']
-
 def U(x):
-    global _m, _omega
-    return _m*_omega**2*x**2/2.
+    return x**2/2.
 
 def dUdx(x):
-    global _m, _omega
-    return _m*_omega**2*x
+    return 2.*x
