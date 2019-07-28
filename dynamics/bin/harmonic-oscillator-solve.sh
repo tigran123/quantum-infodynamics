d8d98948a85bb9c2e78afadc3c950e9165ee243d
Tigran Aivazian <aivazian.tigran@gmail.com>
2019-07-28 11:59:39 +0100

    Save stable baseline parameters

--- dynamics/bin/harmonic-oscillator-solve.sh
+++ dynamics/bin/harmonic-oscillator-solve.sh
@@ -2,7 +2,7 @@ function sigint() {
    echo -n "SIGINT: "
    pkill -P $$
    echo "killed all child processes"
-   exit
+   exit 1
 }
 
 trap sigint SIGINT
@@ -17,15 +17,17 @@ SOLCR=$workdir/solcr
 SOLQN=$workdir/solqn
 SOLCN=$workdir/solcn
 
-PARAMS="-x0 0.0 -x1 -6.0 -x2 6.0 \
-        -p0 3.0 -p1 -7.0 -p2 7.0 \
+PARAMS="-adaptive 1 -tol 0.01 \
+        -mm 1 -mmsize 32 \
+        -x0 0.0 -x1 -6.0 -x2 6.0 \
+        -p0 2.0 -p1 -7.0 -p2 7.0 \
         -sigmax 0.3 -sigmap 0.6 \
         -Nx 512 -Np 512 \
-        -t1 0.0 -t2 24 -N 120 \
+        -t1 0.0 -t2 24 -N 100 \
         -u U_harmonic"
 
-./solve.py -r -d "Quantum Relativistic Oscillator" $PARAMS -tol 0.01 -s $SOLQR &
-./solve.py -d "Quantum Non-relativistic Oscillator" $PARAMS -tol 0.01 -s $SOLQN &
-./solve.py -r -c -d "Classical Relativistic Oscillator" $PARAMS -tol 0.02 -s $SOLCR &
-./solve.py -c -d "Classical Non-relativistic Oscillator" $PARAMS -tol 0.01 -s $SOLCN &
+./solve.py -r -d "\$H(x,p)=\sqrt{1+p^2}+x^2\$ (Q)" $PARAMS -s $SOLQR &
+./solve.py -d "\$H(x,p)=p^2/2+x^2\$ (Q)" $PARAMS -s $SOLQN &
+./solve.py -r -c -d "\$H(x,p)=\sqrt{1+p^2}+x^2\$ (C)" $PARAMS -s $SOLCR &
+./solve.py -c -d "\$H(x,p)=p^2/2+x^2\$ (C)" $PARAMS -s $SOLCN &
 wait
