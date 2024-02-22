2a95d2f1a51532778d0b51488bc91621bb81b4ed
Tigran Aivazian <aivazian.tigran@gmail.com>
2024-02-22 18:34:20 +0000

    Set dpi=100 for the 4k screens and add a comment

--- classical-mechanics/pendulum/psim.py
+++ classical-mechanics/pendulum/psim.py
@@ -340,7 +340,7 @@ pw = PlotWindow(geometry = settings.value('plot_geometry'), state = settings.val
 
 if anim_save:
     anim_running = True
-    pw.ani.save('pendulum.mp4', dpi=150, fps=30, extra_args=['-vcodec', 'libx264'])
+    pw.ani.save('pendulum.mp4', dpi=100, fps=30, extra_args=['-vcodec', 'libx264']) # on 4k screen pass dpi=100 to get FHD mp4
     sys.exit() # bypass our own main_exit() to avoid messing up plotting window state
 else:
     if args.cw: cw = ControlWindow(geometry = settings.value('control_geometry'), state = settings.value('control_windowState'))
