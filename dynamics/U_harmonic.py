5c90984079f5a28b5437bc9149e94eb7b7484ad0
Tigran Aivazian <aivazian.tigran@gmail.com>
2024-03-15 08:45:18 +0000

    Don't use the global mass/frequency parameters

--- dynamics/U_harmonic.py
+++ dynamics/U_harmonic.py
@@ -1,5 +1,12 @@
+#mass = 1.0 # kg
+#omega = 1.0 # rad/s
+
 def U(x):
+    #global mass, omega
+    #return mass * omega**2 * x**2/2
     return x**2
 
 def dUdx(x):
+    #global mass, omega
+    #return mass * omega**2 * x
     return 2.*x
